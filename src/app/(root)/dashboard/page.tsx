eaa90459b1aa10735cdf7096c94049e3b4342e0b
rafig <r.hajili@abysshub.com>
2024-03-01 20:15:30 +0400

    c

--- src/app/(root)/dashboard/page.tsx
+++ src/app/(root)/dashboard/page.tsx
@@ -1,3 +1,38 @@
+"use client";
+
+import { Button as ButtonAria, FileTrigger } from "react-aria-components";
+import { Button } from "@nextui-org/react";
+import { twMerge } from "tailwind-merge";
+
 export default function Page() {
-  return <div>Dashboard</div>;
+  return (
+    <div className="[&_h1]:text-3xl [&_h1]:font-bold [&_h2]:mt-12 [&_h2]:text-2xl [&_h2]:font-bold">
+      <h1>Dashboard</h1>
+
+      <h2>Submit your image</h2>
+
+      <p>Click the button below to upload your image.</p>
+
+      <div className="mt-6 space-y-6">
+        <div>
+          <FileTrigger>
+            <ButtonAria
+              className={({ isHovered, isPressed, isFocusVisible }) =>
+                twMerge(
+                  "h-32 w-full rounded-xl border border-dashed border-default-700 bg-default-200 outline-none duration-200",
+                  isHovered && "bg-default-300",
+                  isPressed && "bg-default-400",
+                  isFocusVisible && "outline outline-offset-2 outline-primary",
+                )
+              }
+            >
+              Upload your image
+            </ButtonAria>
+          </FileTrigger>
+        </div>
+
+        <Button color="primary">Upload</Button>
+      </div>
+    </div>
+  );
 }
